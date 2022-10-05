c3fad4c8ffde285e0d7de88ca8979dc079187e12
Katie Sullivan <SullivanKE@my.lanecc.edu>
2022-10-05 11:34:39 -0700

    Lab 2 getting some pages set up

---
+++ KatieSite/KatieSite/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KatieSite
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reviews()
+        {
+            return View();
+        }
+    }
+}
