225c363c193182efac53b6f66b0c77b2d4181548
Katie Sullivan <SullivanKE@my.lanecc.edu>
2022-10-10 14:14:30 -0700

    TTRPG Model added

---
+++ KatieSite/KatieSite/Models/TTRPGModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace KatieSite.Models
+{
+    public class TTRPGModel
+    {
+        public string name { get; set; }
+        public string publisher { get; set; }
+        public  DateTime publishedDate { get; set; }
+        public string genre { get; set; }
+        public string focus { get; set; }
+        public int easeOfAccess { get; set; }
+        public int stars { get; set; }
+        public string rating { get; set; }
+        public string description { get; set; }
+        public string review { get; set; }
+
+    }
+}
