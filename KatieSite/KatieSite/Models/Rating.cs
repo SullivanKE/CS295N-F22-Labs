1ae60ab23b84398eb28e1225bfc401dacdba8de8
SullivanKE <102761752+SullivanKE@users.noreply.github.com>
2023-01-21 14:09:33 -0800

    attempt 2

--- KatieSite/KatieSite/Models/Rating.cs
+++ KatieSite/KatieSite/Models/Rating.cs
@@ -5,6 +5,8 @@ namespace KatieSite.Models
 {
     public class Rating
     {
+        [Key]
+        public int Id { get; set; }
         public int PostRating { get; set; }
         public string Url { get; set; }
 
